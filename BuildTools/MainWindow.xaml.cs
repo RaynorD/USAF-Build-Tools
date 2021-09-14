a2323648cbaa2f57c3cd78fa9e5a04dfa3ce2933
David <David@ARCTURUS>
2021-09-13 23:36:09 -0500

    Add project files.

---
+++ BuildTools/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace BuildTools
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MenuItemVersion_Click(object sender, RoutedEventArgs e)
+		{
+			BuildVersionWindow win = new();
+			win.Show();
+		}
+	}
+}
